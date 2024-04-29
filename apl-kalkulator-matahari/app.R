27682a91d09101cb689c4cdce8ec845d9b8c45ed
ydkristanto <yosepdwikristanto@usd.ac.id>
2024-04-29 18:09:00 +0200

    Update kalkulator matahari app

--- apl-kalkulator-matahari/app.R
+++ apl-kalkulator-matahari/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(bslib)
 library(tidyverse)
 library(plotly)
+library(scales)
 library(DT)
 
 # Data ----
@@ -26,6 +27,7 @@ daftar_statistik <- c(
   "Jarak matahari (ribu km)" = "SN_mil_km"
 )
 daftar_statistik_simpel <- c(
+  "Ringkasan" = "ringkasan",
   "Lama hari" = "day_length",
   "Lama malam" = "night_length",
   "Perbedaan siang malam" = "diff",
@@ -65,28 +67,32 @@ ui <- page_navbar(
           "statistik",
           div("Statistik:", style = "font-weight: bold;"),
           daftar_statistik_simpel,
-          selected = "day_length"
+          selected = "ringkasan"
         ),
-        selectInput(
-          "periode",
-          div("Periode:", style = "font-weight: bold;"),
-          c(
-            "Harian" = "hari",
-            "Bulanan" = "bulan"
-          ),
-          selected = "hari"
+        conditionalPanel(
+          "input.statistik != 'ringkasan'",
+          selectInput(
+            "periode",
+            div("Periode:", style = "font-weight: bold;"),
+            c(
+              "Harian" = "hari",
+              "Bulanan" = "bulan"
+            ),
+            selected = "hari"
+          )
         ),
         icon = shiny::icon("chart-line")
       ),
       ### Filter ----
       accordion_panel(
         title = "Filter",
-        selectInput(
+        selectizeInput(
           "kota",
           div("Kota:", style = "font-weight: bold;"),
           nama_kota,
-          selected = c("denpasar", "linz"),
-          multiple = TRUE
+          selected = c("denpasar", "perth"),
+          multiple = TRUE,
+          options = list(maxItems = 3)
         ),
         dateRangeInput(
           "interval",
@@ -158,7 +164,18 @@ ui <- page_navbar(
         ),
         card_body(
           p(
-            "Aplikasi ini menggunakan data yang diunduh dari ", a("www.timeanddate.com", href = "https://www.timeanddate.com/", target = "_blank"), " pada 22 April 2024."
+            "Aplikasi ini menggunakan data yang diunduh dari ", a("www.timeanddate.com", href = "https://www.timeanddate.com/", target = "_blank"), " pada 22 April 2024. Data tersebut terdiri dari beberapa variabel untuk 13 kota, merentang dari 1 Januari 2021 sampai 31 Desember 2030. Variabel-variabel tersebut adalah sebagai berikut."
+          ),
+          tags$ul(
+            tags$li(tags$b("Lama hari:"), " Lamanya hari (dalam jam) dari matahari terbit sampai terbenam."),
+            tags$li(tags$b("Lama malam:"), " Lamanya malam (dalam jam) dari matahari terbenam sampai terbit."),
+            tags$li(tags$b("Perbedaan siang malam:"), " Selisih antara lama siang dan malam."),
+            tags$li(tags$b("Matahari terbit:"), " Waktu terbitnya matahari."),
+            tags$li(tags$b("Matahari terbenam:"), " Waktu terbenamnya matahari."),
+            tags$li(tags$b("Fajar:"), " Waktu sebelum matahari terbit, ketika pusat geometris matahari 6° di bawah cakrawala. (Pengertian ini mengacu pada ", tags$i("civil twilight,"), " salah satu ", a("jenis ", tags$i("twilight"), href = "https://www.weather.gov/lmk/twilight-types", target = "_blank"), " dari weather.gov.)"),
+            tags$li(tags$b("Senja:"), " Waktu setelah matahari terbenam, ketika pusat geometris matahari 6° di bawah cakrawala. (Pengertian ini mengacu pada ", tags$i("civil twilight,"), " salah satu ", a("jenis ", tags$i("twilight"), href = "https://www.weather.gov/lmk/twilight-types", target = "_blank"), " dari weather.gov.)"),
+            tags$li(tags$b("Tengah hari:"), " Waktu ketika matahari melalui posisi garis bujur (meridian) dan mencapai posisi tertingginya di langit. Untuk banyak kasus, tengah hari ini tidak terjadi pukul 12:00 PM."),
+            tags$li(tags$b("Jarak matahari:"), " Jarak suatu tempat terhadap matahari (dalam ribuan km).")
           )
         )
       )
@@ -209,10 +226,111 @@ server <- function(input, output, session) {
     data
   })
   
+  dat_ringkasan <- reactive({
+    kota <- input$kota
+    min_tanggal <- input$interval[1]
+    maks_tanggal <- input$interval[2]
+    
+    # Merapikan data
+    data <- sun_data %>% 
+      select(
+        country, city, date, sunrise_time, sunset_time,
+        AST_start, AST_end, NT_start, NT_end, CT_start, CT_end, SN_time
+      ) %>% 
+      filter(
+        city %in% kota,
+        date >= min_tanggal,
+        date <= maks_tanggal
+      ) %>% 
+      rename(
+        day_start = sunrise_time,
+        day_end = sunset_time
+      ) %>% 
+      mutate(
+        day_start = hms::parse_hm(day_start),
+        day_end = hms::parse_hm(day_end),
+        CT_start = hms::parse_hm(CT_start),
+        CT_end = hms::parse_hm(CT_end),
+        NT_start = hms::parse_hm(NT_start),
+        NT_end = hms::parse_hm(NT_end),
+        SN_time = hms::parse_hm(SN_time)
+      )
+    
+    # Menambahkan event ----
+    malam1_dat <- data %>% 
+      select(country, city, date, NT_start, SN_time) %>% 
+      mutate(
+        start = hms::parse_hm("00:00"),
+        event = rep("Malam", nrow(data))
+      ) %>% 
+      rename(end = NT_start)
+    fajar_bhr_dat <- data %>% 
+      select(country, city, date, NT_start, CT_start, SN_time) %>% 
+      mutate(
+        event = rep("Fajar Bahari", nrow(data))
+      ) %>% 
+      rename(start = NT_start, end = CT_start)
+    fajar_dat <- data %>% 
+      select(country, city, date, CT_start, day_start, SN_time) %>% 
+      mutate(
+        event = rep("Fajar", nrow(data))
+      ) %>% 
+      rename(start = CT_start, end = day_start)
+    siang_dat <- data %>% 
+      select(country, city, date, day_start, day_end, SN_time) %>% 
+      mutate(event = rep("Siang", nrow(data))) %>% 
+      rename(start = day_start, end = day_end)
+    senja_dat <- data %>% 
+      select(country, city, date, day_end, CT_end, SN_time) %>% 
+      mutate(
+        event = rep("Senja", nrow(data))
+      ) %>% 
+      rename(start = day_end, end = CT_end)
+    senja_bhr_dat <- data %>% 
+      select(country, city, date, CT_end, NT_end, SN_time) %>% 
+      mutate(
+        event = rep("Senja Bahari", nrow(data))
+      ) %>% 
+      rename(start = CT_end, end = NT_end)
+    malam2_dat <- data %>% 
+      select(country, city, date, NT_end, SN_time) %>% 
+      mutate(
+        end = hms::parse_hm("24:00"),
+        event = rep("Malam 2", nrow(data))
+      ) %>% 
+      rename(start = NT_end)
+    
+    data_rapi <- bind_rows(
+      malam1_dat,
+      fajar_bhr_dat,
+      fajar_dat,
+      siang_dat,
+      senja_dat,
+      senja_bhr_dat,
+      malam2_dat
+    ) %>% 
+      mutate(
+        city = str_to_title(str_replace(city, "-", " ")),
+        event = factor(
+          event,
+          c(
+            "Malam", "Fajar Bahari", "Fajar", "Siang",
+            "Senja", "Senja Bahari", "Malam 2"
+          )
+        )
+      ) %>% 
+      select(country, city, date, event, start, end, SN_time) %>% 
+      arrange(country, city, date)
+    
+    data_rapi
+    
+  })
+  
   ## Diagram ----
   output$diagram <- renderPlotly({
     periode <- input$periode
     statistik <- input$statistik
+    kota <- input$kota
     teks <- names(daftar_statistik)[which(daftar_statistik %in% statistik)]
     judul_x <- ifelse(
       periode == "hari",
@@ -222,13 +340,146 @@ server <- function(input, output, session) {
     palet_warna <- c("#E32D91", "#4EA6DC", "#4775E7", "#8971E1", "#C830CC")
     
     # Menyiapkan data
-    if(periode == "hari") {
+    ### Diagram matahari ----
+    if(statistik == "ringkasan") {
+      if (length(kota) >= 2) {
+        plot <- dat_ringkasan() %>% 
+          ggplot(
+            aes(x = date, ymin = start, ymax = end, fill = event)
+          ) + 
+          geom_ribbon() + 
+          geom_point(
+            aes(
+              x = date, y = start, color = city,
+              text = sprintf(
+                "<b>Kota</b>: %s<br>Tanggal: %s<br>Waktu: %s",
+                city, date, hms::as_hms(start)
+              )
+            ),
+            alpha = 0
+          ) + 
+          geom_line(
+            aes(
+              x = date, y = SN_time, group = city,
+              text = sprintf(
+                "<b>Kota</b>: %s<br>Tanggal: %s<br>Waktu: %s",
+                city, date, hms::as_hms(SN_time)
+              )
+            ),
+            color = "yellow"
+          ) +
+          scale_fill_manual(
+            name = NULL,
+            values = c(
+              "Malam" = "#2f464e",
+              "Fajar Bahari" = "#5a7781",
+              "Fajar" = "#86a9b5",
+              "Siang" = "#b1dae8",
+              "Senja" = "#86a9b5",
+              "Senja Bahari" = "#5a7781",
+              "Malam 2" = "#2f464e"
+            )
+          ) + 
+          scale_y_time(
+            limits = c(hms::parse_hm("00:00"), hms::parse_hm("24:00")),
+            labels = scales::label_time(format = "%H:%M"),
+            breaks = hms::hms(hours = seq(0, 24, 2))
+          ) + 
+          facet_wrap(vars(city), nrow = 1) + 
+          theme_minimal() + 
+          theme(
+            legend.position = "bottom",
+            strip.text.x = element_text(
+              size = 12, face = "bold"
+            )
+          ) + 
+          labs(
+            x = "Tanggal",
+            y = "Waktu"
+          )
+      } else if((length(kota) == 1)) {
+        plot <- dat_ringkasan() %>% 
+          ggplot(
+            aes(x = date, ymin = start, ymax = end, fill = event)
+          ) + 
+          geom_ribbon() + 
+          geom_point(
+            aes(
+              x = date, y = start, color = city,
+              text = sprintf(
+                "<b>Kota</b>: %s<br>Tanggal: %s<br>Waktu: %s",
+                city, date, hms::as_hms(start)
+              )
+            ),
+            alpha = 0
+          ) + 
+          geom_line(
+            aes(
+              x = date, y = SN_time, group = city,
+              text = sprintf(
+                "<b>Kota</b>: %s<br>Tanggal: %s<br>Waktu: %s",
+                city, date, hms::as_hms(SN_time)
+              )
+            ),
+            color = "yellow"
+          ) +
+          scale_fill_manual(
+            name = NULL,
+            values = c(
+              "Malam" = "#2f464e",
+              "Fajar Bahari" = "#5a7781",
+              "Fajar" = "#86a9b5",
+              "Siang" = "#b1dae8",
+              "Senja" = "#86a9b5",
+              "Senja Bahari" = "#5a7781",
+              "Malam 2" = "#2f464e"
+            )
+          ) + 
+          scale_y_time(
+            limits = c(hms::parse_hm("00:00"), hms::parse_hm("24:00")),
+            labels = scales::label_time(format = "%H:%M"),
+            breaks = hms::hms(hours = seq(0, 24, 2))
+          ) + 
+          theme_minimal() + 
+          theme(
+            legend.position = "bottom"
+          ) + 
+          labs(
+            x = "Tanggal",
+            y = "Waktu"
+          )
+      } else if((length(kota) == 0)) {
+        plot <- dat_ringkasan() %>% 
+          ggplot() + 
+          geom_blank() +
+          scale_y_time(
+            limits = c(hms::parse_hm("00:00"), hms::parse_hm("24:00")),
+            labels = scales::label_time(format = "%H:%M"),
+            breaks = hms::hms(hours = seq(0, 24, 2))
+          ) + 
+          theme_minimal() + 
+          theme(
+            legend.position = "bottom"
+          ) + 
+          labs(
+            x = "Tanggal",
+            y = "Waktu"
+          )
+      }
+    } else if(periode == "hari" && statistik != "ringkasan") {
+      ## Diagram pencar ----
       plot <- dat() %>% 
-        select(date, statistik, city) %>% 
+        select(date, tidyselect::all_of(statistik), city) %>% 
         mutate(city = str_to_title(city)) %>% 
         ggplot(aes(x = date, y = .data[[statistik]])) +
         geom_point(
-          aes(color = city),
+          aes(
+            color = city,
+            text = sprintf(
+              "<b>Kota: %s</b><br>Tanggal: %s<br>%s: %s",
+              city, date, teks, .data[[statistik]]
+            )
+          ),
           size = 2,
           alpha = .2
         ) +
@@ -245,7 +496,7 @@ server <- function(input, output, session) {
       nama_var <- paste0(statistik, "_mean")
       
       data <- dat() %>% 
-        select(date, all_of(statistik), city) %>% 
+        select(date, tidyselect::all_of(statistik), city) %>% 
         drop_na() %>% 
         mutate(bulan = floor_date(date, "month")) %>%
         group_by(city, bulan) %>%
@@ -254,7 +505,13 @@ server <- function(input, output, session) {
       plot <- data %>% 
         ggplot(aes(x = bulan, y = .data[[statistik]])) +
         geom_point(
-          aes(color = city),
+          aes(
+            color = city,
+            text = sprintf(
+              "<b>Kota: %s</b><br>Bulan: %s<br>%s: %s",
+              city, bulan, teks, .data[[statistik]]
+            )
+          ),
           size = 3,
           alpha = .8
         ) +
@@ -269,17 +526,22 @@ server <- function(input, output, session) {
         )
       
     }
-    
-    ggplotly(plot) %>% 
-      layout(
-        legend = list(
-          orientation = 'h', y = 100, xanchor = "center", x = .5
+    if (statistik != "ringkasan") {
+      ggplotly(plot, tooltip = c("text")) %>% 
+        layout(
+          legend = list(
+            orientation = 'h', y = 100, xanchor = "center", x = .5
+          )
         )
-      )
+    } else {
+      ggplotly(plot, tooltip = c("text")) %>% 
+        style(showlegend = FALSE)
+    }
+    
   })
   
   ## Data table ----
-  dat_table <- reactive({
+  tabel_dat <- reactive({
     periode <- input$periode
     kota <- input$kota
     min_tanggal <- input$interval[1]
@@ -356,9 +618,38 @@ server <- function(input, output, session) {
     
   })
   
+  tabel_dat_ringkasan <- reactive({
+    dat_ringkasan() %>% 
+      mutate(
+        country = ifelse(
+          country %in% c("usa", "uk"),
+          toupper(country),
+          str_to_title(country)
+        ),
+        event = ifelse(
+          event == "Malam 2",
+          "Malam",
+          as.character(event)
+        )
+      ) %>% 
+    rename(
+      Negara = country,
+      Kota = city,
+      Tanggal = date,
+      Kejadian = event,
+      Mulai = start,
+      Berakhir = end,
+      `Tengah Hari` = SN_time
+    )
+  })
+  
   ## Tabel ----
   output$tabel <- renderDT(
-    dat_table(),
+    if (input$statistik == "ringkasan") {
+      tabel_dat_ringkasan()
+    } else {
+      tabel_dat()
+    },
     options = list(
       pageLength = 5
     )
--- reprex/app.R
+++ reprex/app.R
@@ -1,98 +1,47 @@
 library(shiny)
-library(plotly)
-if (FALSE) {
-    library(igraph)
-}
-
-# Data ----
-actors <- data.frame(
-    name = 1:5,
-    label = c("Alice", "Bob", "Cecil", "David", "Esmeralda"),
-    age = c(48, 33, 45, 34, 21),
-    gender = c("F", "M", "F", "M", "F")
-)
-relations <- data.frame(
-    from = c(2, 3, 3, 4, 4, 5),
-    to = c(1, 2, 1, 1, 2, 1),
-    same.dept = c(FALSE, FALSE, TRUE, FALSE, FALSE, TRUE),
-    friendship = c(4, 5, 5, 2, 1, 1), advice = c(4, 5, 5, 4, 2, 3)
-)
-G <- graph_from_data_frame(
-    relations, directed = FALSE, vertices = actors
-)
-L <- layout_nicely(G)
+library(suncalc)
 
 # UI ----
 ui <- fluidPage(
-    titlePanel("Reprex: Network Analysis"),
+    titlePanel("Reproducible Example"),
     sidebarLayout(
         sidebarPanel(
-            selectInput(
-                "nodes",
-                "Nodes:",
-                c(letters[1:4]),
-                selected = letters[2:4],
-                multiple = TRUE,
-                selectize = TRUE
+            numericInput(
+                "lat",
+                "Latitude:",
+                value = -3.7,
+                min = -90,
+                max = 90
+            ),
+            numericInput(
+                "lon",
+                "Longitude:",
+                value = 128.2,
+                min = -180,
+                max = 180
             )
         ),
         mainPanel(
-           plotlyOutput("plot")
+           tableOutput("tabel")
         )
     )
 )
 
 # Server ----
 server <- function(input, output) {
-    output$plot <- renderPlotly({
-        ## Create Vertices and Edges ----
-        vs <- V(G)
-        es <- as.data.frame(get.edgelist(G))
-        
-        Nv <- length(vs)
-        Ne <- length(es[1]$V1)
-        
-        ## Create Nodes ----
-        Xn <- L[,1]
-        Yn <- L[,2]
-        
-        network <- plot_ly(
-            x = ~Xn, y = ~Yn, mode = "markers",
-            text = vs$label, hoverinfo = "text"
+    output$tabel <- renderTable({
+        dat <- getSunlightTimes(
+            date = seq(
+                from = Sys.Date(),
+                to = as.Date("2024-12-31"),
+                by = "month"
+            ),
+            lat = input$lat,
+            lon = input$lon,
+            keep = c("sunrise", "sunset")
         )
         
-        ## Creates Edges ----
-        edge_shapes <- list()
-        for(i in 1:Ne) {
-            v0 <- es[i,]$V1 |>
-                as.numeric()
-            v1 <- es[i,]$V2 |>
-                as.numeric()
-            
-            edge_shape = list(
-                type = "line",
-                line = list(color = "#030303", width = 0.3),
-                x0 = Xn[v0],
-                y0 = Yn[v0],
-                x1 = Xn[v1],
-                y1 = Yn[v1]
-            )
-            
-            edge_shapes[[i]] <- edge_shape
-        }
-        
-        ## Create Network ----
-        axis <- list(title = "", showgrid = FALSE, showticklabels = FALSE, zeroline = FALSE)
-        
-        fig <- layout(
-            network,
-            title = 'Karate Network',
-            shapes = edge_shapes,
-            xaxis = axis,
-            yaxis = axis
-        )
-        
-        fig
+        dat
     })
 }
 
